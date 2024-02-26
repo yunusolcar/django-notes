f83eae27480c7d29273885b332619b2c8a55060b
yunus <yunusolcar@hotmail.com>
2024-02-26 10:42:21 +0300

    ImageField

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils.text import slugify
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    image = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="blogs")
     description = models.TextField()
     is_active = models.BooleanField(default=False)
     is_home = models.BooleanField(default=False)
