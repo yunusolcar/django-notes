8cd2557d912b7f866bad4f68a869a0405f1da585
yunus <yunusolcar@hotmail.com>
2024-02-28 15:38:20 +0300

    login

---
+++ account/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import redirect, render
+
+# Create your views here.
+def login_request(request):
+    return render(request,"account/login.html")
+
+def register_request(request):
+    return render(request,"account/register.html")
+
+def logout_request(request):
+    return redirect("home")
